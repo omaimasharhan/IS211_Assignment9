7d75e1d3a04211ac4bdaa824889ddd7261897a0b
Omaima <omaimasharhan@gmail.com>
2023-10-31 17:17:09 -0400

    update

--- apple_stock.py
+++ apple_stock.py
@@ -1,2 +1,27 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://finance.yahoo.com/quote/AAPL/history?p=AAPL"
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+response = requests.get(url, headers=headers)
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find('table', {'data-test': 'historical-prices'})
+
+    if table:
+        rows = table.find_all('tr')
+        for row in rows[1:]:
+            cells = row.find_all('td')
+
+            if len(cells) >= 5:
+                date = cells[0].get_text()
+                close_price = cells[4].get_text()
+                print(f"Date: {date}, Close Price: {close_price}")
+            else:
+                print("Insufficient data in the row.")
+    else:
+        print("Historical prices table not found.")
+else:
+    print("Failed to retrieve the web page.")
--- football_stats.py
+++ football_stats.py
@@ -1,2 +1,28 @@
-if __name__ == "__main__":
-    pass
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.cbssports.com/nfl/stats/player/passing/nfl/regular/qualifiers/?sortcol=td&sortdir=descending"
+response = requests.get(url)
+
+if response.status_code == 200:
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    data_container = soup.find('div', {'class': 'TableBase'})
+
+    if data_container:
+        stats_table = data_container.find('table')
+
+        if stats_table:
+            for row in stats_table.find_all('tr')[1:21]:
+                cells = row.find_all('td')
+                player_name = cells[0].get_text()
+                player_position = cells[1].get_text()
+                player_team = cells[2].get_text()
+                player_touchdowns = cells[3].get_text()
+                print(f"Player: {player_name}, Position: {player_position}, Team: {player_team}, Touchdowns: {player_touchdowns}")
+        else:
+            print("Player statistics table not found.")
+    else:
+        print("Data container not found.")
+else:
+    print("Failed to retrieve the web page.")
